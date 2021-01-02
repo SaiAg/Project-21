718955af438699922b626c34b3bb09481704256c
SaiAg <75775249+SaiAg@users.noreply.github.com>
2021-01-02 12:51:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var wall,thickness;
+var bullet,speed,weight;
+
+
+function setup(){
+createCanvas(1600,400)
+thickness=random(22,83);
+wall=createSprite(1200,200,thickness,height/2);
+wall.shapeColor=color(80,80,80);
+
+speed=random(100,200);
+weight=random(30,52);
+bullet=createSprite(50,200,50,10);
+bullet.velocityX=speed;
+
+
+
+}
+
+function draw(){
+background(0);
+
+  
+  hasCollided(bullet,wall);
+  if (hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+    damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+  
+      if (damage>10){
+        wall.shapeColor=color(225,0,0);
+      }
+      if (damage<10){
+  wall.shapeColor=color(0,225,0);
+      }
+  
+  }
+  drawSprites();
+  
+}
+
+function hasCollided(bullet,wall){
+  bulletRightEdge=bullet.x+bullet.width;
+  wallLeftEdge=wall.x;
+  if (bulletRightEdge>=wallLeftEdge){
+    return true
+  }
+  return false;
+  }
+
+  
+
